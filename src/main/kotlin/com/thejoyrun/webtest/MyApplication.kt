cf5ef09f72619db40ca35eb41e0ef18963d1071a
Wiki <taoweiji2008@qq.com>
2018-04-27 00:31:38 +0800

    first

--- src/main/kotlin/com/thejoyrun/webtest/MyApplication.kt
+++ src/main/kotlin/com/thejoyrun/webtest/MyApplication.kt
@@ -4,11 +4,11 @@ import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 
 @SpringBootApplication
-open class HexiApplication {
+open class MyApplication {
 
 
 }
 
 fun main(args: Array<String>) {
-    SpringApplication.run(HexiApplication::class.java, *args)
+    SpringApplication.run(MyApplication::class.java, *args)
 }
