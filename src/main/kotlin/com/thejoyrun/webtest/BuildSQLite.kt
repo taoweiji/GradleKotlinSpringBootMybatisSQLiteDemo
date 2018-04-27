1ac296257bcbd245f499fe97e5c61128deed7ae6
Wiki <taoweiji2008@qq.com>
2018-04-27 17:48:29 +0800

    加入打包逻辑

--- src/main/kotlin/com/thejoyrun/webtest/BuildSQLite.kt
+++ src/main/kotlin/com/thejoyrun/webtest/BuildSQLite.kt
@@ -1,30 +1,30 @@
-package com.thejoyrun.webtest
-
-import java.sql.DriverManager
-
-
-/**
- * 创建数据库文件，并创建表，请右击运行
- * Created by Wiki on 2018/4/27.
- */
-
-fun main(args: Array<String>) {
-    //连接SQLite的JDBC
-    Class.forName("org.sqlite.JDBC")
-    //建立一个数据库名example.db的连接，如果不存在就在当前目录下创建之
-    val conn = DriverManager.getConnection("jdbc:sqlite:src/main/resources/example.db")
-    val stat = conn.createStatement()
-    // //创建一个表,包含id、name两个字段
-    stat.executeUpdate("create table user(id int,name varchar(20));")
-    // 插入一行数据
-    stat.executeUpdate("insert into user values(1,'Wiki1');")
-    stat.executeUpdate("insert into user values(2,'Wiki2');")
-    //查询所有数据
-    val rs = stat.executeQuery("select * from user;")
-    while (rs.next()) { //将查询到的数据打印出来
-        print("id = " + rs.getString("id") + " ")
-        println("name = " + rs.getString("name"))
-    }
-    rs.close()
-    conn.close()
-}
+//package com.thejoyrun.webtest
+//
+//import java.sql.DriverManager
+//
+//
+///**
+// * 创建数据库文件，并创建表，请右击运行
+// * Created by Wiki on 2018/4/27.
+// */
+//
+//fun main(args: Array<String>) {
+//    //连接SQLite的JDBC
+//    Class.forName("org.sqlite.JDBC")
+//    //建立一个数据库名example.db的连接，如果不存在就在当前目录下创建之
+//    val conn = DriverManager.getConnection("jdbc:sqlite:src/main/resources/example.db")
+//    val stat = conn.createStatement()
+//    // //创建一个表,包含id、name两个字段
+//    stat.executeUpdate("create table user(id int,name varchar(20));")
+//    // 插入一行数据
+//    stat.executeUpdate("insert into user values(1,'Wiki1');")
+//    stat.executeUpdate("insert into user values(2,'Wiki2');")
+//    //查询所有数据
+//    val rs = stat.executeQuery("select * from user;")
+//    while (rs.next()) { //将查询到的数据打印出来
+//        print("id = " + rs.getString("id") + " ")
+//        println("name = " + rs.getString("name"))
+//    }
+//    rs.close()
+//    conn.close()
+//}
